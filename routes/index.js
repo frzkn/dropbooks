7a1b370db90dacdfa6f3f6ba7ee05051b6d39edf
Faraz Khan <farazrk001@gmail.com>
2018-11-06 18:56:23 +0000

    Final commit 2

--- routes/index.js
+++ routes/index.js
@@ -30,22 +30,22 @@ router.get('/login', function(req, res, next) {
 });
 
 
-router.post('/login', function(req, res) {
-  let loginEmail = req.body.email;
-  let loginPassword = req.body.password;
-  connection.query(`select * from users where email="${req.body.email}"` , (err, results, fields) => {
-    if (err) {
-      console.log("mysql error:" + err);
-    }
+// router.post('/login', function(req, res) {
+//   let loginEmail = req.body.email;
+//   let loginPassword = req.body.password;
+//   connection.query(`select * from users where email="${req.body.email}"` , (err, results, fields) => {
+//     if (err) {
+//       console.log("mysql error:" + err);
+//     }
 
-    if (results[0].email != loginEmail || results[0].password != loginPassword) {
-      return res.render('login', {loginStatus: false, viewName: ""});
-    }
-    else {
-      req.userSession.userId = results[0].id;
-      return res.redirect('/send'); }
-  });
-});
+//     if (results[0].email != loginEmail || results[0].password != loginPassword) {
+//       return res.render('login', {loginStatus: false, viewName: ""});
+//     }
+//     else {
+//       req.userSession.userId = results[0].id;
+//       return res.redirect('/send'); }
+//   });
+// });
 
 
 router.get('/register', function(req, res, next) {
@@ -125,54 +125,54 @@ router.get('/send', function(req, res, next) {
   });
 });
 
-router.post('/send', function(req, res, next){
-  console.log("Files lies below!!!");
-  console.log(req.files.book);
+// router.post('/send', function(req, res, next){
+//   console.log("Files lies below!!!");
+//   console.log(req.files.book);
 
-  var toEmail;
+//   var toEmail;
 
-  connection.query(`select * from users where id="${req.userSession.userId}"` , (err, results, fields) => {
-    if (err) {
-      console.log("mysql error:" + err);
-    }
-    toEmail = results[0].kindle_email;
-    console.log('sending email to ' + toEmail);
+//   connection.query(`select * from users where id="${req.userSession.userId}"` , (err, results, fields) => {
+//     if (err) {
+//       console.log("mysql error:" + err);
+//     }
+//     toEmail = results[0].kindle_email;
+//     console.log('sending email to ' + toEmail);
 
-    // TODO implement convert feature for PDFs
+//     // TODO implement convert feature for PDFs
 
-    // nodemailer
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'dropbookskindle@gmail.com',
-        pass: 'comeonsupermansayyourstupidline'
-      }
-    });
-    // get user's kindle email address here
-    const mailOptions = {
-      from: 'sender@email.com',
-      to: toEmail,
-      subject: req.files.book.name,
-      html: `<h2> Hello this is a drop from dropbooks ! <h2> <br> <p> Regards.</p>`,
-      attachments: [
-        {
-          filename: req.files.book.name,
-          content: new Buffer(req.files.book.data)
-        }
-      ]
-    };
+//     // nodemailer
+//     var transporter = nodemailer.createTransport({
+//       service: 'gmail',
+//       auth: {
+//         user: 'dropbookskindle@gmail.com',
+//         pass: ''
+//       }
+//     });
+//     // get user's kindle email address here
+//     const mailOptions = {
+//       from: 'sender@email.com',
+//       to: toEmail,
+//       subject: req.files.book.name,
+//       html: `<h2> Hello this is a drop from dropbooks ! <h2> <br> <p> Regards.</p>`,
+//       attachments: [
+//         {
+//           filename: req.files.book.name,
+//           content: new Buffer(req.files.book.data)
+//         }
+//       ]
+//     };
 
-    transporter.sendMail(mailOptions, function (err, info){
-      if(err)
-        throw err;
-      else
-        console.log(info);
+//     transporter.sendMail(mailOptions, function (err, info){
+//       if(err)
+//         throw err;
+//       else
+//         console.log(info);
 
-    });
+//     });
 
-  });
-  return res.redirect('/');
-});
+//   });
+//   return res.redirect('/');
+// });
 
 router.get('/team', function(req, res, next) {
   res.render('team',{ viewName: "", loginStatus: false });
